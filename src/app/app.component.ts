adbec144896ad606dd72a89e7d5aa565cafa45ed
Justin <heartleyjm@gmail.com>
2024-02-03 20:03:09 -0500

    FEAT: calculate and display time since start of stream

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: []
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'BarberBeatBox';
+  timeUTC: string;
+  timeSinceStartInMilliseconds: number;
+  ngOnInit() {
+    const timeRightNow = new Date();
+    this.timeUTC = timeRightNow.toISOString();
+
+    const streamStart = new Date(2023, 10, 20, 19, 1, 9, 69);
+    this.timeSinceStartInMilliseconds = Math.abs(timeRightNow.getTime() - streamStart.getTime());
+  }
 }
