d8b9b7983263295246921fbc44e31c36aa1882e2
Justin <heartleyjm@gmail.com>
2024-05-14 17:44:46 -0400

    FEAT: pass playlist from app.component to navigation-bar

--- src/app/navigation-bar/navigation-bar.component.ts
+++ src/app/navigation-bar/navigation-bar.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Song } from '../shared/song.model';
 
 @Component({
   selector: 'app-navigation-bar',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: []
 })
 export class NavigationBarComponent {
-
+  @Input() playlist: Song[];
 }
